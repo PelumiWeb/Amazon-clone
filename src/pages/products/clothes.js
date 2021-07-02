59027fa8c60b7452700cc6cba6777f94b6af6f78
PelumiWeb <pelumiogundipe905@gmail.com>
2021-07-02 21:26:18 +0100

    initial commit

---
+++ src/pages/products/clothes.js
@@ -0,0 +1,9 @@
+function clothes() {
+    return (
+        <div>
+            The clothes of products
+        </div>
+    )
+}
+
+export default clothes
---
+++ src/pages/search.js
@@ -0,0 +1,9 @@
+function search() {
+    return (
+        <div>
+            This is the search
+        </div>
+    )
+}
+
+export default search
