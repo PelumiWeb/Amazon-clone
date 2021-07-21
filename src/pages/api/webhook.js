0872e8e5d4c13a67b070073129bbea04a6aea5b4
PelumiWeb <pelumiogundipe905@gmail.com>
2021-07-21 11:11:49 +0100

    edited the web-hook file

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,30 +1,30 @@
 import {buffer} from 'micro'
 import * as admin from 'firebase-admin'
 
-// const serviceAccount = require('')
+const serviceAccount = require('../../../permission.json')
 
-// const app = !admin.apps.length ? admin.initializeApp({
-// credentials: admin.credential.cert(serviceAccount)
-// }) : admin.app();
+const app = !admin.apps.length ? admin.initializeApp({
+credentials: admin.credential.cert(serviceAccount)
+}) : admin.app();
 
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET
 
-// const fufillOrder = async (session) => {
-// console.log('fufiliing order', session)
+const fufillOrder = async (session) => {
+console.log('fufiliing order', session)
 
-//     return app.firestore().collection('users').doc(session.metadata.email).collection('orders').doc(session.id)
-//     .set({
-//         amount: session.amount_total / 100,
-//         amount_sipping: session.total_details.amount_shipping / 100,
-//         images: JSON.parse(session.metadata.images),
-//         timestamp: admin.firestore.FieldValue.serverTimestamp()
-//     })
-//     .then(() => {
-//         console.log(`SUCCES: Order ${session.id} has been added to the db` )
-//     })
-// }
+    return app.firestore().collection('users').doc(session.metadata.email).collection('orders').doc(session.id)
+    .set({
+        amount: session.amount_total / 100,
+        amount_sipping: session.total_details.amount_shipping / 100,
+        images: JSON.parse(session.metadata.images),
+        timestamp: admin.firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+        console.log(`SUCCES: Order ${session.id} has been added to the db` )
+    })
+}
 
 export default async (req, res) => {
     if (req.method === 'POST'){
@@ -45,7 +45,7 @@ export default async (req, res) => {
 
         if (event.type === 'checkout.session.completed') {
             const session = event.data.object;
-            // return fufillOrder(session).then(() => res.status(200).catch(err => res.status(400).send('Web-hook error', err.message)))
+            return fufillOrder(session).then(() => res.status(200).catch(err => res.status(400).send('Web-hook error', err.message)))
         }
 
     } 
